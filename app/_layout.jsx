0dd0d0b8843469df8a5c6d464dbad13e14bc7e1e
Aqib Munir <aqibmunir724@gmail.com>
2026-05-01 12:05:53 +0500

    Done Video 22

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,24 +2,27 @@ import { StyleSheet, Text, useColorScheme, View } from "react-native";
 import { Stack, Slot } from "expo-router";
 import { Colors } from "../constants/Colors";
 import { StatusBar } from "expo-status-bar";
-import { UserProvider } from "../contexts/UserContext"
+import { UserProvider } from "../contexts/UserContext";
+import { BooksProvider } from "../contexts/BooksContext";
 
 const _layout = () => {
   const colorScheme = useColorScheme();
   let theme = Colors[colorScheme] ?? Colors.light; // Fallback to light if null
   return (
     <UserProvider>
-      <StatusBar value="auto" />
-      <Stack
-        screenOptions={{
-          headerStyle: { backgroundColor: theme.navBackground },
-          headerTintColor: theme.title,
-        }}
-      >
-        <Stack.Screen name="index" options={{ title: "Home" }} />
-        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      </Stack>
+      <BooksProvider>
+        <StatusBar value="auto" />
+        <Stack
+          screenOptions={{
+            headerStyle: { backgroundColor: theme.navBackground },
+            headerTintColor: theme.title,
+          }}
+        >
+          <Stack.Screen name="index" options={{ title: "Home" }} />
+          <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        </Stack>
+      </BooksProvider>
     </UserProvider>
   );
 };
---
+++ contexts/BooksContext.jsx
@@ -0,0 +1,47 @@
+import { createContext, useState } from "react";
+
+const DATABASE_ID = "69f449a66ba455090";
+const COLLECTION_ID = "books";
+
+export const BooksContext = createContext();
+
+export function BooksProvider({ children }) {
+  const [books, setBooks] = useState([]);
+
+  async function fetchBooks() {
+    try {
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  async function fetchBookById(id) {
+    try {
+      return response;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async function createBook(data) {
+    try {
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async function deleteBook(id) {
+    try {
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  return (
+    <BooksContext.Provider
+      value={{ books, fetchBooks, fetchBookById, createBook, deleteBook }}
+    >
+      {children}
+    </BooksContext.Provider>
+  );
+}
